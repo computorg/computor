5d648ddb8502566614d54f6cbde1894e68b97435
Robin Genuer <robin.genuer@u-bordeaux.fr>
2020-11-19 11:51:28 +0100

    add github_setting function that configure the github action on the repository

---
+++ R/computo_github_setting.R
@@ -0,0 +1,6 @@
+#' @import usethis
+
+github_setting <- function() {
+  usethis::use_github_action("computorticle_action",
+                             url = "https://github.com/computorg/actions")
+}
