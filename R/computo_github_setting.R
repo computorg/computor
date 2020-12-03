a168bb0ccaba82c15c3bdce4c74cd2e323191f4d
Robin Genuer <robin.genuer@u-bordeaux.fr>
2020-12-03 10:33:09 +0100

    initialize binder folder before gh action

--- R/computo_github_setting.R
+++ R/computo_github_setting.R
@@ -3,6 +3,19 @@
 #' @export
 
 github_setting <- function() {
-  usethis::use_github_action("computorticle_action",
+  # initianilizing the binder folder, used by the github action after
+  if (!dir.exists("binder")) {
+    dir.create("binder")
+  }
+  if (!file.exists("binder/apt.txt")) download.file(
+    "https://raw.githubusercontent.com/computorg/actions/main/apt.txt",
+    "binder/apt.txt")
+  if (!file.exists("binder/environment.yml")) download.file(
+    "https://raw.githubusercontent.com/computorg/actions/main/environment.yml",
+    "binder/environment.yml")
+  if (!file.exists("binder/install.R")) download.file(
+    "https://raw.githubusercontent.com/computorg/actions/main/install.R",
+    "binder/install.R")
+  usethis::use_github_action("computorticle_action.yaml",
                              url = "https://github.com/computorg/actions")
 }
